7fc022c63e1ba4eaf68eda1c6c4e6656cd00410f
Alexander Morozov <Alexander95223@yandex.ru>
2023-10-15 05:16:56 +0300

    feat(front): changed start page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('registration');
 });
 
 Route::get('/home', [MainUserController::class, 'home']);
